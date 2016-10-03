227f8c2d6207f05b9afa52c545a0408a75ef3ffa
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-03 14:18:22 -0600

    Add factory for creating ideas in tests

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,9 @@
 FactoryGirl.define do
-  # Add individual factories here - https://github.com/thoughtbot/factory_girl
+  factory :idea do
+    title
+    body Faker::Hacker.say_something_smart
+  end
+
+  # Use of n-1 due to sequence starting at 2 instead of 1 (or 0)
+  sequence(:title) { |n| "Idea ##{n - 1}" }
 end
