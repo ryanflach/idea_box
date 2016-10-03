8562fc2e45a92a0fe1fc200f6f05d51d9a21cbd0
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-03 14:18:53 -0600

    Add requests spec directory

---
+++ spec/requests/request_ideas_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Request ideas', type: :request do
+  it 'gets all ideas' do
+    create_list(:idea, 2)
+
+    expect(Idea.count).to eq(2)
+    expect(Idea.first.id).to eq(1)
+    expect(Idea.first.title).to eq('Idea #1')
+
+    get '/api/v1/ideas'
+
+    ideas = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+    expect(response.content_type).to eq('application/json')
+    expect(ideas.count).to eq(2)
+    expect(ideas.first).to have_key('title')
+    expect(ideas.first).to have_key('body')
+    expect(ideas.first).to have_key('quality')
+    expect(ideas.first).to_not have_key('id')
+    expect(ideas.first['title']).to eq('Idea #1')
+  end
+end
