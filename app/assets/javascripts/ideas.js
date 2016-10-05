d35f149f231df9310f001a25ecc710b2db6a0a7b
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-04 20:06:20 -0600

    Add ability to update quality of an idea - Implemented via thumbs-up and thumbs-down buttons

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -4,7 +4,55 @@ $(document).ready(function(){
   $('#ideas').on('click', 'input', function(event){
     deleteIdea(event.target.id);
   });
+  $('#ideas').on('click', 'button', function(event){
+    updateIdea(event.target)
+  })
 });
+
+var reRenderIdea = function(ideaHTML) {
+  var id = this.url.split('/').splice(4, 5).join('')
+  $('#idea-' + id).replaceWith(ideaHTML)
+}
+
+var processUpdate = function(id, updateData) {
+  $.ajax({
+    url: '/api/v1/ideas/' + id,
+    method: 'put',
+    data: updateData
+  }).then(createIdeaHTML)
+  .then(reRenderIdea)
+  .fail(handleError);
+};
+
+var qualityUpdate = function(type, id, currentQuality) {
+  var newQuality;
+  
+  if (type === 'up') {
+    if (currentQuality === 'swill') {
+      newQuality = 'plausible'
+    } else if (currentQuality === 'plausible') {
+      newQuality = 'genius'
+    }
+  } else {
+    if (currentQuality === 'genius') {
+      newQuality = 'plausible'
+    } else if (currentQuality === 'plausible') {
+      newQuality = 'swill'
+    }
+  }
+
+  processUpdate(id, { quality: newQuality });
+};
+
+var updateIdea = function(targetData) {
+  var idData = targetData.id.split('-');
+  var buttonType = idData[0];
+  var id = idData[1];
+  var currentQuality = $('#idea-' + id).children('.quality').text();
+  if (buttonType !== 'submit') {
+    qualityUpdate(buttonType, id, currentQuality);
+  }
+};
 
 var removeIdeaHTML = function(id) {
   $('#idea-' + id).remove();
@@ -13,8 +61,7 @@ var removeIdeaHTML = function(id) {
 var deleteIdea = function(id) {
   $.ajax({
     url: '/api/v1/ideas/' + id,
-    type: 'delete',
-    data: id
+    type: 'delete'
   }).then(removeIdeaHTML(id))
   .fail(handleError);
 };
@@ -53,6 +100,33 @@ var limit100Words = function(text) {
   return text.split(' ').splice(0, 100).join(' ');
 };
 
+var createThumbsButton = function(type, idea) {
+  var disabledStatus;
+  var id;
+
+  if ((idea.quality === 'genius' && type === 'Thumbs Up') || (idea.quality === 'swill' && type === 'Thumbs Down')) {
+    disabledStatus = "disabled='true'"
+  } else {
+    disabledStatus = ''
+  }
+
+  if (type === 'Thumbs Up') {
+    id = 'up-' + idea.id
+  } else {
+    id = 'down-' + idea.id
+  }
+
+  return (
+    "<button type='button' class='btn btn-success btn-xs' id='"
+    + id
+    + "'"
+    + disabledStatus
+    + "'>"
+    + type
+    +"</button>"
+  );
+};
+
 var createIdeaHTML = function(idea) {
   return(
     "<div class='idea well' id='idea-"
@@ -64,12 +138,14 @@ var createIdeaHTML = function(idea) {
     + "<p>"
     + limit100Words(idea.body)
     + "</p>"
-    + "<h6>"
+    + "<h6 class='quality'>"
     + idea.quality
     + "</h6>"
     + "<input class='btn btn-danger' id='"
     + idea.id
     + "' type='button' name='delete' value='Delete'>"
+    + createThumbsButton('Thumbs Down', idea)
+    + createThumbsButton('Thumbs Up', idea)
     + "</div>"
   );
 };
